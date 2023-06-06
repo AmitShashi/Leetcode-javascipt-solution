e395cc4073131964f3c8bda7ed9fe920f25cb3d3
AmitShashi <73923245+AmitShashi@users.noreply.github.com>
2023-06-06 18:12:36 +0530

    Create canMakeArithmeticProgression.js

---
+++ canMakeArithmeticProgression.js
@@ -0,0 +1,8 @@
+function canMakeArithmeticProgression(arr: number[]): boolean {
+    if (arr.length < 3) return true;
+    
+    arr.sort((a, b) => a - b);
+    for (let i = 2; i < arr.length; i++)
+        if (arr[i] - arr[i - 1] !== arr[i - 1] - arr[i - 2]) return false;
+    return true;
+};
